2afd9ccd3cc9ac2bdcf623bd79e606f6a7eb0611
keita0301 <x19g032@mitsuhashi.ac.jp>
2021-12-06 13:23:31 +0900

    Initial commit

---
+++ app/src/main/java/com/example/dbtest/MainActivity.kt
@@ -0,0 +1,124 @@
+package com.example.dbtest
+
+import android.content.ContentValues.TAG
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    //    private lateinit var firebaseAnalytics: FirebaseAnalytics
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val Tv : TextView = findViewById(R.id.Tv)
+
+//        firebaseAnalytics = Firebase.analytics
+
+        // Access a Cloud Firestore instance from your Activity
+
+        val db = Firebase.firestore
+
+        // Create a new user with a first and last name
+//        val user = hashMapOf(
+//            "first" to "Ada",
+//            "last" to "Lovelace",
+//            "born" to 1815
+//        )
+//
+//        // Add a new document with a generated ID
+//        db.collection("users")
+//            .add(user)
+//            .addOnSuccessListener { documentReference ->
+//                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+//            }
+//            .addOnFailureListener { e ->
+//                Log.w(TAG, "Error adding document", e)
+//            }
+//        val user = hashMapOf(
+//            "first" to "Alan",
+//            "middle" to "Mathison",
+//            "last" to "Turing",
+//            "born" to 1912
+//        )
+//
+//          // Add a new document with a generated ID
+//        db.collection("users")
+//            .add(user)
+//            .addOnSuccessListener { documentReference ->
+//                Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+//            }
+//            .addOnFailureListener { e ->
+//                Log.w(TAG, "Error adding document", e)
+//            }
+
+//        db.collection("users")
+//            .get()
+//            .addOnSuccessListener { result ->
+//                for (document in result) {
+//                    Log.d(TAG, "${document.id} => ${document.data}")
+//                    Tv.text = document.data.toString()
+//                }
+//            }
+//            .addOnFailureListener { exception ->
+//                Log.w(TAG, "Error getting documents.", exception)
+//            }
+
+        val cities = db.collection("cities")
+
+        val data1 = hashMapOf(
+            "name" to "San Francisco",
+            "state" to "CA",
+            "country" to "USA",
+            "capital" to false,
+            "population" to 860000,
+            "regions" to listOf("west_coast", "norcal")
+        )
+        cities.document("SF").set(data1)
+
+        val data2 = hashMapOf(
+            "name" to "Los Angeles",
+            "state" to "CA",
+            "country" to "USA",
+            "capital" to false,
+            "population" to 3900000,
+            "regions" to listOf("west_coast", "socal")
+        )
+        cities.document("LA").set(data2)
+
+        val data3 = hashMapOf(
+            "name" to "Washington D.C.",
+            "state" to null,
+            "country" to "USA",
+            "capital" to true,
+            "population" to 680000,
+            "regions" to listOf("east_coast")
+        )
+        cities.document("DC").set(data3)
+
+        val data4 = hashMapOf(
+            "name" to "Tokyo",
+            "state" to null,
+            "country" to "Japan",
+            "capital" to true,
+            "population" to 9000000,
+            "regions" to listOf("kanto", "honshu")
+        )
+        cities.document("TOK").set(data4)
+
+        val data5 = hashMapOf(
+            "name" to "Beijing",
+            "state" to null,
+            "country" to "China",
+            "capital" to true,
+            "population" to 21500000,
+            "regions" to listOf("jingjinji", "hebei")
+        )
+        cities.document("BJ").set(data5)
+    }
+}
